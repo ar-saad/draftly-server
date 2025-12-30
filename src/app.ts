5f4bfbd21bbd5852e09cf1a2d48ef6dd32aa697e
ar-saad <arsaad.dev@gmail.com>
2025-12-30 11:48:10 +0600

    feat: add user authentication with better-auth and nodemailer integration
    - Removed old migration files and created new migrations for user authentication.
    - Better Auth modified schema.prisma to include User, Session, Account, and Verification models.
    - Added additional fields (role, phone, status) to the User model.
    - Implemented email verification functionality in auth.ts using nodemailer.
    - Configured CORS in app.ts and set up authentication routes.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
 import express, { Application, Request, Response } from "express";
 import { PostRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app: Application = express();
+
+// CORS config
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:5000",
+    credentials: true,
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 // Parser
 app.use(express.json());
